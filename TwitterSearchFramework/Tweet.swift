d4f7943fdd7d61cb1d90fcf62e790b0710798a49
Einav Sitton <Einav@Einavs-MacBook-Pro-2.local>
2017-01-02 15:19:30 +0200

    Tweet struct - to be continue

---
+++ TwitterSearchFramework/Tweet.swift
@@ -0,0 +1,15 @@
+//
+//  Tweet.swift
+//  TwitterSearchFramework
+//
+//  Created by Einav Sitton on 01/01/2017.
+//  Copyright © 2017 EinavSitton. All rights reserved.
+//
+
+import Foundation
+
+
+struct Tweet {
+	let favorited : Bool
+	let createdAt : Date
+}
